5fd9eda2d2e01dbb94bf8a591203557808d2fef2
Andrea Zemignani <andrea.zemignani@acsoftware.it>
2025-10-18 17:19:08 +0200

    initial commit

--- src/main/java/com/green/energy/tracker/cloud/statistics_service/events/ControllerReceiveEvents.java
+++ src/main/java/com/green/energy/tracker/cloud/statistics_service/events/ControllerReceiveEvents.java
@@ -1,18 +1,17 @@
 package com.green.energy.tracker.cloud.statistics_service.events;
 
-import com.google.events.cloud.firestore.v1.DocumentEventData;
-import com.google.firestore.v1.Document;
 import com.google.protobuf.InvalidProtocolBufferException;
 import com.green.energy.tracker.cloud.statistics_service.model.GlobalStatistics;
 import com.green.energy.tracker.cloud.statistics_service.service.GlobalStatisticsService;
+import io.cloudevents.spring.http.CloudEventHttpUtils;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.nio.charset.StandardCharsets;
 import java.util.concurrent.ExecutionException;
 
 @RestController
@@ -23,8 +22,9 @@ public class ControllerReceiveEvents{
     private final GlobalStatisticsService globalStatisticsService;
 
     @PostMapping("/sites-events")
-    public ResponseEntity<GlobalStatistics> handleSitesFirestoreEvent(@RequestBody byte[] event) throws ExecutionException, InterruptedException, InvalidProtocolBufferException {
-        log.info("RAW event: {}", new String(event, StandardCharsets.UTF_8));
+    public ResponseEntity<GlobalStatistics> handleSitesFirestoreEvent(@RequestHeader HttpHeaders headers, @RequestBody byte[] event) throws ExecutionException, InterruptedException, InvalidProtocolBufferException {
+        var cloudEvent = CloudEventHttpUtils.toReader(headers, ()-> event).toEvent();
+        log.info("Received site event: {}", cloudEvent);
         return ResponseEntity.ok(globalStatisticsService.updateGlobalStatisticsFromEvent());
     }
 
