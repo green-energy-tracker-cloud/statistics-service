dc5e2bb342d8c7bc0cd571ea0ead310fe800d6ba
Andrea Zemignani <andrea.zemignani@acsoftware.it>
2025-10-19 10:36:20 +0200

    initial commit

--- src/main/java/com/green/energy/tracker/cloud/statistics_service/utils/CloudEventUtils.java
+++ src/main/java/com/green/energy/tracker/cloud/statistics_service/utils/CloudEventUtils.java
@@ -1,5 +1,6 @@
 package com.green.energy.tracker.cloud.statistics_service.utils;
 
+import com.google.events.cloud.datastore.v1.EntityEventData;
 import com.google.events.cloud.firestore.v1.DocumentEventData;
 import com.google.protobuf.InvalidProtocolBufferException;
 import com.google.protobuf.util.JsonFormat;
@@ -12,8 +13,8 @@ import java.util.Objects;
 @Slf4j
 public class CloudEventUtils {
 
-    public static DocumentEventData getDocumentEventDataFromCloudEvent(CloudEvent cloudEvent) throws InvalidProtocolBufferException {
-        var documentEventData = DocumentEventData.parseFrom(Objects.requireNonNull(cloudEvent.getData()).toBytes());
+    public static EntityEventData getDocumentEventDataFromCloudEvent(CloudEvent cloudEvent) throws InvalidProtocolBufferException {
+        var documentEventData = EntityEventData.parseFrom(Objects.requireNonNull(cloudEvent.getData()).toBytes());
         log.info("Parsed DocumentEventData from CloudEvent: {}", documentEventData);
         return documentEventData;
     }
